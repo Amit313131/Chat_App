05d805b6775aeea575e7cb04cc1f4eb09d8e2da8
Amit313131 <amitguptagupta060@gmail.com>
2025-01-25 22:57:13 +0530

    change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import {BrowserRouter} from 'react-router-dom'
 import ChatProvider from './Context/ChatProvider'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'https://chat-app-1-blyr.onrender.com';
 
 createRoot(document.getElementById('root')).render(
   <ChatProvider>
